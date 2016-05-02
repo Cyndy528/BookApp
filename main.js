b256da31170fd5ef06d057df50ad0791e4a5927f
Cynthia Salinas <csalinas123@gmail.com>
2016-05-01 17:02:00 -0700

    update form

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(function(){
 	
 	// base API Route
-	var baseUrl= "https://super-crud.herokuapp.com/books";
+	var baseUrl = "https://super-crud.herokuapp.com/books";
 	
 	// array to hold book data from API
 	var bookStore = [];
@@ -31,7 +31,6 @@ $(function(){
 
 	// GET all books on the page load
 	$.get(baseUrl, function(data){
-		console.log(data);
 
 	// set 'allBooks' to the book data from API
 	allBooks = data.books;
@@ -40,7 +39,80 @@ $(function(){
 	render();
 	});
 
+	// listen for submit 
+	$createBook.on('submit', function(event){
+		event.preventDefault();
 
+		//serialize form data
+		var newBook = $(this).serialize();
 
+		//POST request to create a new book
+		$.post(baseUrl, newBook, function(data){
 
+			//render all books to view
+			render();
+		});
+
+		//reset the form 
+		$createBook[0].reset();
+		$createBook.find('input').first().focus();
+	});
+
+	// add event-handlers to books for updating/deleting
+	$booksList
+		//for update: submit eveon on '.update-book' form
+		.on('submit', '.update-book', function(event){
+			event.preventDefault();
+
+		// find the book's id (stored in HTML as 'data-id')
+		var bookId = $(this).closest('.book').attr('data-id');
+
+		//find the book to update by its id
+		var bookToUpdate = allBooks.filter(function (book){
+			return book._id == bookId;
+		})[0];
+
+		//serialize the form data
+		var updatedBook = $(this).serialize();
+
+		// PUT request to update book
+		$.ajax({
+			type: 'PUT', 
+			url: baseUrl + '/' + bookId, 
+			data: updatedBook, 
+			success: function(data){
+				//replace book to update with newly updated version(data)
+				allBooks.splice(allBooks.indexOf(bookToUpdate), 1, data);
+
+				// render all books to view
+				render();	
+			}
+		});
+	});
+
+	// for delete: click event on '.delete-book' button
+	$('form').on('submit', function(event){
+		event.preventDefault();
+
+		// find the book's id (stored in HTML as 'data-id')
+		var  bookId = $(this).closest('.book').attr('data-id');
+
+		//find the book to delete by its id
+		var bookToDelete = allBooks.filter(function (book){
+			return book._id == bookId;
+		})[0];
+
+		// DELETE request to delete book
+		$.ajax({
+			type: 'DELETE', 
+			url: baseUrl + '/' + bookId, 
+			success: function(data){
+				// remove delted book, from all books
+				allBooks.splice(allBooks.indexOf(bookToDelete), 1);
+
+				// render all the boosk to view
+				render();
+			}
+		});
+	});
 });
